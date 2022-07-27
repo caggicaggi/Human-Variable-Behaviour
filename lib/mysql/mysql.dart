c5d96360405d1fea7e154a2341a2aa07d29ab669
FrancescoMalaccari98 <francescomala98@gmai.com>
2022-07-27 10:51:46 +0200

    funzione diari consolo inserimento giornata

--- lib/mysql/mysql.dart
+++ lib/mysql/mysql.dart
@@ -1,5 +1,13 @@
 import 'dart:async';
+//import 'dart:html';
+import 'package:flutter/material.dart';
+import 'package:human_variable_behaviour/Screens/Application/Diario/components/MyEvents.dart';
+import 'package:intl/intl.dart';
 import 'package:mysql1/mysql1.dart';
+
+import '../Screens/Login/components/body.dart';
+
+var idUtente = '';
 
 class Mysql {
   //Cotruttore
@@ -10,9 +18,9 @@ class Mysql {
         host: '10.0.2.2',
         port: 3306,
         user: 'root',
-        password: 'root',
+        //password: 'root',
         //Database mala
-        //password: 'rootroot',
+        password: 'rootroot',
         db: 'databaseappflutter');
     return MySqlConnection.connect(settings);
   }
@@ -41,8 +49,22 @@ void signUpToDb(nameToDb, surnameToDb, emailToDb, passwordToDb) {
       ')';
   //Connessione al database
   var db = Mysql();
-  db.getConnection().then((connessione) {
+
+  db.getConnection().then((connessione) async {
+    await Future.delayed(const Duration(seconds: 2));
+    //Inserisco le info nel database
     connessione.query(query);
+    //Leggo l'idUtente appena assegnato
+    String queryToId =
+        'select idutente FROM ' + table + ' WHERE email = ' + emailToDb;
+    print(queryToId);
+    await connessione.query(queryToId).then((results) {
+      for (var res in results) {
+        idUtente = res[0].toString();
+        debugPrint(idUtente);
+      }
+    });
+    await Future.delayed(const Duration(seconds: 2));
     connessione.close();
   });
 }
@@ -67,6 +89,23 @@ Future<bool> readEmailPasswordFromDb(emailToReadToDb, passwordToDb) async {
   //Aggiunti delay
   await Future.delayed(const Duration(milliseconds: 2));
   var result = await connessione.query(query);
+  db.getConnection().then((connessione) async {
+    await Future.delayed(const Duration(seconds: 2));
+    //Inserisco le info nel database
+    connessione.query(query);
+    //Leggo l'idUtente appena assegnato
+    String queryToId =
+        'select idutente FROM ' + table + ' WHERE email = ' + emailToReadToDb;
+    print(queryToId);
+    await connessione.query(queryToId).then((results) {
+      for (var res in results) {
+        idUtente = res[0].toString();
+        debugPrint(idUtente);
+      }
+    });
+    await Future.delayed(const Duration(seconds: 2));
+    connessione.close();
+  });
   connessione.close();
   //True = Query vuota -> Non ho la mail
   //False = Query con valore di ritorno -> Email già presente
@@ -76,7 +115,6 @@ Future<bool> readEmailPasswordFromDb(emailToReadToDb, passwordToDb) async {
 //Funzione per controllare l'esistenza della mail
 Future<bool> readEmailFromDb(emailToReadToDb) async {
   emailToReadToDb = stringToDb(emailToReadToDb);
-
   //Nome della tabella
   String table = 'utenti';
   //Scrivo la query
@@ -94,6 +132,41 @@ Future<bool> readEmailFromDb(emailToReadToDb) async {
   //True = Query vuota -> Non ho la mail
   //False = Query con valore di ritorno -> Email già presente
   return result.isEmpty;
+}
+
+//funzione per inserire giornata utente
+void signDataAndGiornata(
+    idutente, dataGiornata, titoloGiornata, descrizioneGiornata) {
+  //Aggiungo i '' a tutte le stringhe passate in input
+  idutente = idutente;
+  titoloGiornata = stringToDb(titoloGiornata);
+  descrizioneGiornata = stringToDb(descrizioneGiornata);
+  dataGiornata = dataGiornata.toLocal();
+  dataGiornata = DateFormat('yyyy-MM-dd').format(dataGiornata);
+  print(dataGiornata);
+  //Nome della tabella
+  String table = 'diarioUtente';
+  //Scrivo la query
+  // ignore: prefer_interpolation_to_compose_strings
+  String query =
+      '${'INSERT INTO ' + table + ' (idutente,dataGiornata, titoloGiornata, descrizioneGiornata) VALUES (' + idutente + ',' + "'" + dataGiornata + "'" + ',' + titoloGiornata + ',' + descrizioneGiornata})';
+  //Connessione al database
+  debugPrint(query);
+  var db = Mysql();
+  db.getConnection().then((connessione) {
+    connessione.query(query);
+    connessione.close();
+  });
+}
+
+// metodo invocato quando si clicca sulla data del calendario
+List<MyEvents> listaGiornateInserite(DateTime? selectedCalendarDate, idutente) {
+  List<MyEvents> list = [];
+  //print("VARIABILI:::::::::::::::::::");
+  //print(selectedCalendarDate?.toLocal());
+  // FARE QUERY PER IDUTENTE E ORARIO CHE RITORNA LISTA
+// Here the List should be returned, but after my Function fills it.
+  return list;
 }
 
 //Funzione per aggiungere i '' alle stringhe passate in input
